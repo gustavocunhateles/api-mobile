30d434c2b65f2f7987a9369c403ae8b2aa08d8f0
Gustavo Cunha Teles <42899628+gustavocunhateles@users.noreply.github.com>
2019-11-25 19:43:26 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+import AppContainer from './src/components';
+import './src/config/reactotron-config';
+
+const App = () => {
+  return (
+    <AppContainer />
+  );
+}
+
+export default App;
